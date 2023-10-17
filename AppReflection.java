7b2a3ef0fbb2f941c26a6c48ff66bae22388727d
Pedro Cunegundes <bangue062@gmail.com>
2023-10-17 09:54:04 -0300

    Primeiro commit

---
+++ AppReflection.java
@@ -0,0 +1,28 @@
+import java.lang.annotation.Annotation;
+
+public class AppReflection {
+
+
+
+    public static void main(String[] args) {
+
+
+        Class<ClassAnnotation> classe = ClassAnnotation.class;
+
+        Annotation[] annotations = classe.getAnnotations();
+
+        for (Annotation annotation : annotations) {
+
+            if (annotation instanceof Tabela) {
+
+                Tabela tabelaAnnotation = (Tabela) annotation;
+                String nomeDaTabela = tabelaAnnotation.nome();
+                System.out.println("Nome da tabela: " + nomeDaTabela);
+
+            }
+
+        }
+
+    }
+
+}
---
+++ ClassAnnotation.java
@@ -0,0 +1,5 @@
+@Tabela(nome = "esta é a tabela")
+
+   public class ClassAnnotation {
+
+}
