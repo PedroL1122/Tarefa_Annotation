82029c19f0561717858abbbd78e5960a2ae729a2
Pedro Cunegundes <95534653+PedroL1122@users.noreply.github.com>
2023-10-30 08:11:20 -0300

    Update ClassAnnotation.java

--- ClassAnnotation.java
+++ ClassAnnotation.java
@@ -1,5 +1,57 @@
-@Tabela(nome = "esta é a tabela")
+@Tabela(nome = "Tabela Pessoa")
 
-   public class ClassAnnotation {
+   public class Person {
+ String nome;
+    String sobrenome;
+    int idade;
+    int cpf;
 
+    public Person(String nome, String sobrenome, int idade, int cpf) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.idade = idade;
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenoma) {
+        this.modelo = modelo;
+    }
+
+    public int getIdade() {
+        return Idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public Double getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(Double cpf) {
+        this.cpf = cpf;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "nome='" + nome + '\'' +
+                ", sobrenome='" + sobrenome + '\'' +
+                ", idade=" + idade +
+                ", cpf=" + cpf +
+                '}';
+    }
 }
